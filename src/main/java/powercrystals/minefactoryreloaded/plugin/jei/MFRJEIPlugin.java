503f0d6466e0629ea03d37c3bd79caebb95c59df
atferrys <61624086+atferrys@users.noreply.github.com>
2026-08-11 13:51:11 +0200

    feat: Implement Lava Fabricator recipe category

--- src/main/java/powercrystals/minefactoryreloaded/plugin/jei/MFRJEIPlugin.java
+++ src/main/java/powercrystals/minefactoryreloaded/plugin/jei/MFRJEIPlugin.java
@@ -3,6 +3,7 @@ package powercrystals.minefactoryreloaded.plugin.jei;
 import mezz.jei.api.*;
 import mezz.jei.api.recipe.IRecipeCategoryRegistration;
 import powercrystals.minefactoryreloaded.plugin.jei.laserdrill.LaserDrillRecipeCategory;
+import powercrystals.minefactoryreloaded.plugin.jei.lavafabricator.LavaFabricatorRecipeCategory;
 import powercrystals.minefactoryreloaded.plugin.jei.sludgeboiler.SludgeBoilerRecipeCategory;
 
 import javax.annotation.Nonnull;
@@ -11,6 +12,7 @@ import javax.annotation.Nonnull;
 public class MFRJEIPlugin implements IModPlugin {
 
     private MachineRecipeCategory<?> laserDrillCategory;
+    private MachineRecipeCategory<?> lavaFabricatorCategory;
     private MachineRecipeCategory<?> sludgeBoilerCategory;
 
     @Override
@@ -20,6 +22,7 @@ public class MFRJEIPlugin implements IModPlugin {
         IGuiHelper guiHelper = helpers.getGuiHelper();
 
         registry.addRecipeCategories(laserDrillCategory = new LaserDrillRecipeCategory(guiHelper));
+        registry.addRecipeCategories(lavaFabricatorCategory = new LavaFabricatorRecipeCategory(guiHelper));
         registry.addRecipeCategories(sludgeBoilerCategory = new SludgeBoilerRecipeCategory(guiHelper));
 
     }
@@ -27,6 +30,7 @@ public class MFRJEIPlugin implements IModPlugin {
     @Override
     public void register(@Nonnull IModRegistry registry) {
         laserDrillCategory.registerRecipesHandling(registry);
+        lavaFabricatorCategory.registerRecipesHandling(registry);
         sludgeBoilerCategory.registerRecipesHandling(registry);
     }
 
