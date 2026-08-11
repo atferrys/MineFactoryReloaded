edfdb0f83c1c9b319185a41280114394081f62db
atferrys <61624086+atferrys@users.noreply.github.com>
2026-08-11 23:10:04 +0200

    feat: Add Sewer widgets and tooltip explanations

--- src/main/java/powercrystals/minefactoryreloaded/plugin/jei/sewer/SewerRecipeCategory.java
+++ src/main/java/powercrystals/minefactoryreloaded/plugin/jei/sewer/SewerRecipeCategory.java
@@ -6,6 +6,8 @@ import mezz.jei.api.gui.IDrawable;
 import mezz.jei.api.gui.IGuiFluidStackGroup;
 import mezz.jei.api.gui.IRecipeLayout;
 import mezz.jei.api.ingredients.IIngredients;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.I18n;
 import net.minecraftforge.fluids.FluidStack;
 import powercrystals.minefactoryreloaded.plugin.jei.MachineRecipeCategory;
 import powercrystals.minefactoryreloaded.setup.MFRFluids;
@@ -19,12 +21,16 @@ import java.util.List;
 public class SewerRecipeCategory extends MachineRecipeCategory<SewerRecipeWrapper> {
 
     private final IDrawable background;
+    private final IDrawable arrowWidget;
     private final IDrawable tankOverlay;
 
     private final int primaryTankSize;
     private final int secondaryTankSize;
 
     private static final int BG_Y = 14;
+
+    private final IDrawable primaryWidget;
+    private final IDrawable secondaryWidget;
 
     public SewerRecipeCategory(IGuiHelper guiHelper) {
 
@@ -41,10 +47,28 @@ public class SewerRecipeCategory extends MachineRecipeCategory<SewerRecipeWrappe
                 168, 64
         );
 
+        arrowWidget = guiHelper.createDrawable(
+                widgetsTexture,
+                0, 0,
+                22, 15
+        );
+
         tankOverlay = guiHelper.createDrawable(
                 texture,
                 176, 0,
                 TANK_WIDTH, TANK_HEIGHT
+        );
+
+        primaryWidget = guiHelper.createDrawable(
+                widgetsTexture,
+                0, 32,
+                16, 16
+        );
+
+        secondaryWidget = guiHelper.createDrawable(
+                widgetsTexture,
+                16, 32,
+                16, 16
         );
 
     }
@@ -53,6 +77,11 @@ public class SewerRecipeCategory extends MachineRecipeCategory<SewerRecipeWrappe
     @Nonnull
     public IDrawable getBackground() {
         return background;
+    }
+
+    @Override
+    public void drawExtras(@Nonnull Minecraft minecraft) {
+        arrowWidget.draw(minecraft, 73, 23);
     }
 
     @Override
@@ -76,8 +105,24 @@ public class SewerRecipeCategory extends MachineRecipeCategory<SewerRecipeWrappe
         registry.addRecipeCatalyst(this.machine.getItemStack(), this.getUid());
 
         List<SewerRecipeWrapper> recipes = Arrays.asList(
-                new SewerRecipeWrapper(new FluidStack(MFRFluids.sewage, secondaryTankSize), false),
-                new SewerRecipeWrapper(new FluidStack(MFRFluids.essence, primaryTankSize), true)
+                new SewerRecipeWrapper(
+                        new FluidStack(MFRFluids.essence, primaryTankSize),
+                        true,
+                        primaryWidget,
+                        Arrays.asList(
+                                I18n.format("jei.info.mfr.sewer.essence"),
+                                I18n.format("jei.info.mfr.sewer.essence.1")
+                        )
+                ),
+                new SewerRecipeWrapper(
+                        new FluidStack(MFRFluids.sewage, secondaryTankSize),
+                        false,
+                        secondaryWidget,
+                        Arrays.asList(
+                                I18n.format("jei.info.mfr.sewer.sewage"),
+                                I18n.format("jei.info.mfr.sewer.sewage.1")
+                        )
+                )
         );
 
         registry.addRecipes(recipes, this.getUid());
--- src/main/java/powercrystals/minefactoryreloaded/plugin/jei/sewer/SewerRecipeWrapper.java
+++ src/main/java/powercrystals/minefactoryreloaded/plugin/jei/sewer/SewerRecipeWrapper.java
@@ -1,27 +1,51 @@
 package powercrystals.minefactoryreloaded.plugin.jei.sewer;
 
+import mezz.jei.api.gui.IDrawable;
 import mezz.jei.api.ingredients.IIngredients;
 import mezz.jei.api.recipe.IRecipeWrapper;
+import net.minecraft.client.Minecraft;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.FluidUtil;
 
 import javax.annotation.Nonnull;
+import java.util.Collections;
+import java.util.List;
 
 public class SewerRecipeWrapper implements IRecipeWrapper {
 
     private final FluidStack output;
     private final boolean primary;
+    private final IDrawable widget;
+    private final List<String> widgetTooltip;
 
-    public SewerRecipeWrapper(FluidStack output, boolean primary) {
+    private static final int WIDGET_X = 40, WIDGET_Y = 22;
+
+    public SewerRecipeWrapper(FluidStack output, boolean primary, IDrawable widget, List<String> widgetTooltip) {
         this.output = output;
         this.primary = primary;
+        this.widget = widget;
+        this.widgetTooltip = widgetTooltip;
     }
 
     @Override
     public void getIngredients(@Nonnull IIngredients ingredients) {
         ingredients.setOutput(FluidStack.class, output);
         ingredients.setOutput(ItemStack.class, FluidUtil.getFilledBucket(output));
+    }
+
+    @Override
+    public void drawInfo(@Nonnull Minecraft minecraft, int recipeWidth, int recipeHeight, int mouseX, int mouseY) {
+        widget.draw(minecraft, WIDGET_X, WIDGET_Y);
+    }
+
+    @Override
+    @Nonnull
+    public List<String> getTooltipStrings(int mouseX, int mouseY) {
+        if(mouseX >= WIDGET_X && mouseX < WIDGET_X + widget.getWidth() && mouseY >= WIDGET_Y && mouseY < WIDGET_Y + widget.getHeight()) {
+            return widgetTooltip;
+        }
+        return Collections.emptyList();
     }
 
     public boolean isPrimary() {
